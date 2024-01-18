e93c9652a518f2196c0ec0444e6d2ce16c1141fe
Mohammad Hasim Shaikh <hasims2001@gmail.com>
2024-01-18 18:07:23 +0530

    completed: navbar

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import styles from "@/styles/Navbar.module.css";
+import Link from "next/link";
+const navigations = [
+ 
+    {
+      link: "/about",
+      title: "About"
+    },
+    {
+      link: "/products",
+      title: "Our Products"
+    },
+    {
+      link: "/token",
+      title: "Token Utility"
+    },
+    {
+      link: "/roadmap",
+      title: "Roadmap"
+    }
+  ]
+export default function Navbar(){
+
+    return (
+        <div className={styles.nav_container} >
+          <Link href={"/"}>
+            <div className="flex gap-2">
+        <Image src="/logo.svg" width={50} className="rounded-full" height={50} alt='logo'></Image>
+                <div className="flex flex-col justify-center">
+                <p className="text-yellow200 font-bold tracking-[0.16rem]">BITVERSE</p>
+                <p className="text-white font-bold text-[0.54rem] tracking-[0.63rem]">FINANCE</p>
+                </div>
+
+            </div>
+        </Link>
+        <div className='flex justify-between gap-2.5 items-center' >
+            {navigations.map((item, ind)=> (
+              <Link href={item.link} key={ind} className="px-[24px] py-[16px]">{item.title}</Link>
+            ))}
+            <button className="bg-yellow300 text-black rounded-[0.625rem] px-4 py-2 text-lg">Connect Wallet</button>
+        </div>
+       
+     
+    </div>
+    )
+}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,7 +13,20 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        coffee100: "rgba(92, 79, 5, 0.44)",
+        coffee200: "rgba(219, 184, 0, 0.20)",
+        gray100: "rgba(255, 255, 255, 0.31)",
+        yellow200: "#FFC603",
+        yellow300: "#F9D326",
+        white: '#ffffff', 
+        black: "#000000" 
+      },
+      
     },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+  },
   },
   plugins: [],
 }
